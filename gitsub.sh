708a93ae3b21e88c9ee757e500e3163abab232e9
xopherdeep <heylisten@xtiv.net>
2014-07-03 06:08:48 -0700

    updated repos

--- gitsub.sh
+++ gitsub.sh
@@ -6,13 +6,13 @@ git rm phpThumb -r
 git submodule add https://github.com/xopherdeep/phpThumb phpThumb
 
 git rm smarty -r
-git submodule add -b distribution https://github.com/xopherdeep/smarty smarty --depth 1
+git submodule add -b master https://github.com/xopherdeep/smarty3.1.19 smarty --depth 1
 # cd ./smarty/
 # git filter-branch --subdirectory-filter distribution -f
 # cd ..
  
 git rm Zend -r
-git submodule add -b library-Zend https://github.com/xopherdeep/ZF1 Zend --depth 1
+git submodule add -b master https://github.com/xopherdeep/ZF1 Zend --depth 1
 # cd ./Zend/
 # git filter-branch --subdirectory-filter library/Zend -f
 # cd ..
