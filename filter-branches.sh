6c620be7553a66c87e7d5f13a44ff1d0895d8cb2
xopherdeep <heylisten@xtiv.net>
2014-07-02 19:48:36 -0700

    updated Zend submodule

--- filter-branches.sh
+++ filter-branches.sh
@@ -6,6 +6,11 @@ cd ./smarty/
 git log -1
 git filter-branch --subdirectory-filter distribution -f
 cd ..
+
+
+cd ./Zend/
+git submodule add https://github.com/zendframework/zf1 Zend
+cd ..
 cd ./Zend/
 git log -1
 git filter-branch --subdirectory-filter library/Zend -f
