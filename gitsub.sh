262741eb9a4a12a08e834ef0c561cc2bad2a0523
xopherdeep <heylisten@xtiv.net>
2014-07-03 01:58:41 -0700

    Updated Shell to overwrite submodule directories before attemping to add them

--- gitsub.sh
+++ gitsub.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+git rm x4deep -r
 git submodule add https://github.com/xopherdeep/x4deep x4deep
+
+git rm phpThumb -r
 git submodule add https://github.com/xopherdeep/phpThumb phpThumb
 
+git rm smarty -r
 git submodule add https://github.com/xopherdeep/smarty smarty
 cd ./smarty/
 git filter-branch --subdirectory-filter distribution -f
 cd ..
 
+git rm Zend -r
 git submodule add https://github.com/xopherdeep/Zend-Framework-v1 Zend
 cd ./Zend/
 git filter-branch --subdirectory-filter library/Zend -f
