7faa16e32e7d811c522f931eab4ae46625ecb379
xopherdeep <heylisten@xtiv.net>
2014-07-03 04:42:20 -0700

    Updated Shell * Feature: clone from smarty and zend a lot faster due to pulling from personal branched repo

--- gitsub.sh
+++ gitsub.sh
@@ -7,12 +7,12 @@ git submodule add https://github.com/xopherdeep/phpThumb phpThumb
 
 git rm smarty -r
 git submodule add https://github.com/xopherdeep/smarty smarty
-cd ./smarty/
-git filter-branch --subdirectory-filter distribution -f
-cd ..
+# cd ./smarty/
+# git filter-branch --subdirectory-filter distribution -f
+# cd ..
 
 git rm Zend -r
 git submodule add https://github.com/xopherdeep/Zend-Framework-v1 Zend
-cd ./Zend/
-git filter-branch --subdirectory-filter library/Zend -f
-cd ..
+# cd ./Zend/
+# git filter-branch --subdirectory-filter library/Zend -f
+# cd ..
