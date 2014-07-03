4f437bbe1422fe7a1d1772ec0ac529e2944f9164
xopherdeep <heylisten@xtiv.net>
2014-07-03 04:56:41 -0700

    Changing bigger libs into subtrees

--- gitsub.sh
+++ gitsub.sh
@@ -6,13 +6,17 @@ git rm phpThumb -r
 git submodule add https://github.com/xopherdeep/phpThumb phpThumb
 
 git rm smarty -r
-git submodule add https://github.com/xopherdeep/smarty smarty
+
+
+git subtree add --prefix smarty https://github.com/xopherdeep/smarty distribution --squash
+# git submodule add -b distribution https://github.com/xopherdeep/smarty smarty
 # cd ./smarty/
 # git filter-branch --subdirectory-filter distribution -f
 # cd ..
 
 git rm Zend -r
-git submodule add https://github.com/xopherdeep/Zend-Framework-v1 Zend
+git subtree add --prefix Zend https://github.com/xopherdeep/Zend-Framework-v1 library-Zend --squash
+# git submodule add --squash -b library-Zend https://github.com/xopherdeep/Zend-Framework-v1 Zend
 # cd ./Zend/
 # git filter-branch --subdirectory-filter library/Zend -f
 # cd ..
