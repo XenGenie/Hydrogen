7362409b37e3a96076dcb1d1eeadf45c353692a6
xopherdeep <heylisten@xtiv.net>
2014-07-12 23:54:01 -0700

    Adding Git Hub php Api Lib

--- gitsub.sh
+++ gitsub.sh
@@ -10,22 +10,24 @@ git submodule add https://github.com/xopherdeep/smarty3.1.19 smarty --depth 1
 # cd ./smarty/
 # git filter-branch --subdirectory-filter distribution -f
 # cd ..
- 
 
-	read -p "Download Zend Now? " answer
+git rm github -r
+git submodule add https://github.com/xopherdeep/php-github-api-lib github
+
+read -p "Download Local Copy of Zend? " answer
 # (2) handle the input we were given
-	case $answer in
-		[yY]* ) 
-			echo "Okay, this may take a second..."
-			git rm Zend -r
-			git submodule add https://github.com/xopherdeep/ZF1 Zend --depth 1/usr/bin/wget -O - -q -t 1 http://www.example.com/cron.php           
-			# cd ./Zend/
-			# git filter-branch --subdirectory-filter library/Zend -f
-			# cd ..
-	   	 
-		# [nN]* ) exit;;
-		# * )     echo "Dude, just enter Y or N, please.";;
-	esac
+case $answer in
+	[yY]* ) 
+		echo "Okay, this may take a second..."
+		git rm Zend -r
+		git submodule add https://github.com/xopherdeep/ZF1 Zend --depth 1/usr/bin/wget -O - -q -t 1 http://www.example.com/cron.php           
+		# cd ./Zend/
+		# git filter-branch --subdirectory-filter library/Zend -f
+		# cd ..
+   	 
+	# [nN]* ) exit;;
+	# * )     echo "Dude, just enter Y or N, please.";;
+esac
 
 #  Use this if prefer subtree..
 # git subtree add --prefix smarty https://github.com/xopherdeep/smarty distribution --squash
